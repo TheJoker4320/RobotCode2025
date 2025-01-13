7fbfd9647d1ae04e76d9b71c3b7190b8eabb6467
Yonatan <yonatan.halpern@gmail.com>
2025-01-13 21:30:02 +0200

    [ADD] - Add base swerve code structure

---
+++ src/main/java/frc/robot/subsystems/Swerve/Swerve.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.Swerve;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Swerve extends SubsystemBase {
+    public Swerve() {}
+
+    @Override
+    public void periodic() {}
+}
---
+++ src/main/java/frc/robot/subsystems/Swerve/SwerveModuleFactory.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.Swerve;
+
+public class SwerveModuleFactory {
+    public static ISwerveModule[] generateSwerveModules(SwerveModuleType moduleType) {
+        throw new UnsupportedOperationException("This function hasnt been implemented yet");
+    }
+}
