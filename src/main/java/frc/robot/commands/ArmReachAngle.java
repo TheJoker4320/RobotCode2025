886762ae3d8a7a5f0abbfaaccd0eff2d4dac93aa
Eyal <eyal2177@gmail.com>
2025-01-15 18:20:34 +0200

    [ADD] added ArmReachAngle command

---
+++ src/main/java/frc/robot/commands/ArmReachAngle.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Arm;
+import frc.robot.utils.ArmState;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class ArmReachAngle extends Command {
+  /** Creates a new ArmReachAngle. */
+  private Arm m_arm;
+  private ArmState m_desieredState;
+  public ArmReachAngle(Arm arm, ArmState desiredState) {
+    m_arm = arm;
+    m_desieredState = desiredState; 
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_arm);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_arm.setSetpoint(m_desieredState);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return m_arm.isAtState(m_desieredState);
+  }
+}
